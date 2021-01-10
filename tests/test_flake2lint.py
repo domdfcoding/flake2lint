396e1e281bb2504e9e2fde0c7da08f0714f374d5
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-10 17:20:04 +0000

    Linting.

--- tests/test_flake2lint.py
+++ tests/test_flake2lint.py
@@ -1,8 +1,10 @@
+# 3rd party
 import pytest
 from click.testing import CliRunner, Result
 from domdf_python_tools.testing import check_file_output
 from pytest_regressions.file_regression import FileRegressionFixture
 
+# this package
 from flake2lint import process_file
 from flake2lint.__main__ import main
 
@@ -13,9 +15,9 @@ def example_file(tmp_pathplus):
 
 	example_file.write_lines([
 			"def foo(  # noqa",
-			"	id: int = -1,  # noqa: A002",
-			"	dir: PathLike = '.',  # noqa: A002  # pylint: disable=redefined-builtin",
-			"	): ...",
+			"\tid: int = -1,  # noqa: A002",
+			"\tdir: PathLike = '.',  # noqa: A002  # pylint: disable=redefined-builtin",
+			"\t): ...",
 			])
 
 	return example_file
@@ -32,4 +34,3 @@ def test_cli(example_file, tmp_pathplus, file_regression: FileRegressionFixture)
 	result: Result = runner.invoke(main, catch_exceptions=False, args=str(example_file))
 	assert result.exit_code == 1
 	check_file_output(example_file, file_regression)
-
